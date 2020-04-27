d5023596a7e80f5688fe091eb0fc45d12450099e
ashiqcvcv <ashiqc23@gmail.com>
2020-04-27 11:54:49 +0530

    initial Comit

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { DatabaseService } from '../database.service';
 
 @Component({
@@ -14,19 +14,6 @@ export class NavComponent implements OnInit {
   inHome;
   inList;
   constructor(private router: Router,private databaseService:DatabaseService) {
-    
-    router.events.subscribe((val) => {
-      if (val instanceof ActivationEnd) {
-          this.route =  val.snapshot.component['name'];
-          if(this.route == 'HomeComponent'){
-            this.inHome = true;
-            this.inList = false;
-          }else if(this.route == 'ListComponent'){
-            this.inHome = false;
-            this.inList = true;
-          }
-      }
-  });
   
     this.searchForm = new FormGroup({
       'term' : new FormControl()
